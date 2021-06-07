b19353df3097b547efc1e42e8097fe99cc850fa2
Eddie <eduardo.oliveira@estudante.ufjf.br>
2021-06-07 20:10:10 -0300

    Modifica main.cpp
    cria stringstream pra amrmazenar linha completa de dados .csv

--- main.cpp
+++ main.cpp
@@ -3,20 +3,31 @@
 #include <cstring>
 #include <vector>
 #include <ctime>
+#include <sstream>
 
 #include "artista.h"
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[]) 
 {
+    if(argc < 2) // ./main ./input
+    {
+        cout << "Uso: " << argv[0] << " diretorio arquivo de entrada" << endl;
+        return 0;
+    }
+
+    string dir = argv[1];
+    string artistaCsv = dir + "/artistas.csv";
+    string artistaBin = dir + "/artistas.bin";
+
     srand(time(NULL));
     
     ifstream fileIn;
-    fileIn.open("artistas.csv");
+    fileIn.open(artistaCsv);
 
     ofstream binFileOut;
-    binFileOut.open("artistas.bin", ios::out | ios::binary);
+    binFileOut.open(artistaBin, ios::out | ios::binary);
 
     Artista art1;
     vector<Artista> artV;
@@ -28,35 +39,41 @@ int main()
     {
         string tmp;
         string aux = "";
+        string line;
         fileIn.ignore(1000, '\n'); //ignora linha até encontrar 1000 caracteres ou quebra de linha
 
         cout << "\n\nLeitura de art antes do bin: " << endl << endl;
 
-        while(!fileIn.eof())
+        // while(!fileIn.eof())
+        while(getline(fileIn, line))
         {
-            getline(fileIn, tmp, ',');
+            stringstream sstr(line);
+            
+
+            getline(sstr, tmp, ',');
             strcpy(art1.id, tmp.c_str());
 
-            getline(fileIn, tmp, ',');
+            getline(sstr, tmp, ',');
             art1.followers = atof(tmp.c_str());
 
-            getline(fileIn, tmp, ',');      
+            getline(sstr, tmp, ',');     
 
 
             while(!verificaCaractere(tmp))
             {
                 aux += tmp + ',';
-                getline(fileIn, tmp, ',');   
+                // getline(fileIn, tmp, ','); 
+                getline(sstr, tmp, ',');  
             }
             aux += tmp;
             strcpy(art1.genres, aux.c_str());
             aux = "";
 
 
-            getline(fileIn, tmp, ',');   
+            getline(sstr, tmp, ',');  
             strcpy(art1.name, tmp.c_str());
 
-            getline(fileIn, tmp, '\n');
+            getline(sstr, tmp, ',');
             art1.popularity = atoi(tmp.c_str());
 
 
@@ -76,14 +93,14 @@ int main()
 
     //lê dados aleatórios em artistas.bin e coloca em vetor para leitura
     ifstream binFileIn;
-    binFileIn.open("artistas.bin", ios::in | ios::binary);
+    binFileIn.open(artistaBin, ios::in | ios::binary);
 
     if(binFileIn.is_open())
     {
         Artista art;
         int n; 
         int tam = artista_Size();
-        int k = 1;
+        int k = 0;
         int j;
 
         cout << "\n\ntamanho de strucu Artista: " << tam << endl << endl;
@@ -91,14 +108,15 @@ int main()
 
         cout << "Digite quantos dados vc quer armazenar: ";
         cin >> j;
+        artV.resize(j);
 
         //while(!binFileIn.eof()) estava lendo o último dado 2x
         // while(binFileIn.peek() != EOF)
-        while(k <= j)
+        while(k < j)
         {   
             n = rand()%5;
-            artista_Read(art, binFileIn, n, tam);
-            registra_ArtistaV(artV, art);
+            artista_Read(artV[k], binFileIn, n, tam);
+            //registra_ArtistaV(artV, art);
             k++;
         }
         binFileIn.close();
